40e289b78d6fc9a41a481951931e6a6049ce2092
DDoS <Suphaamongkhon.kh.61@ubu.ac.th>
2020-10-07 01:59:14 +0700

    Updates

---
+++ precache-manifest.cdb5985adf0dc8c06d76d4e65342203b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d97de943e9887d89180ea85d83b2de8",
+    "url": "/Pj_Commath/index.html"
+  },
+  {
+    "revision": "19d01cff94d733c9ca60",
+    "url": "/Pj_Commath/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8527dc79d8794ba1673b",
+    "url": "/Pj_Commath/static/js/2.61a0b0b3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Pj_Commath/static/js/2.61a0b0b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19d01cff94d733c9ca60",
+    "url": "/Pj_Commath/static/js/main.822967e1.chunk.js"
+  },
+  {
+    "revision": "aebac72de20a02e54411",
+    "url": "/Pj_Commath/static/js/runtime-main.c8668987.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Pj_Commath/static/media/logo.5d5d9eef.svg"
+  }
+]);
